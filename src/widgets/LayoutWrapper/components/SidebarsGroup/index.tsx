527d3f48db1551dcdf3e37f44a0ade6daaed1de2
Vladislav Oliinyk <catdev.dev@gmail.com>
2023-02-01 17:33:15 +0200

    added

--- src/widgets/LayoutWrapper/components/SidebarsGroup/index.tsx
+++ src/widgets/LayoutWrapper/components/SidebarsGroup/index.tsx
@@ -1,6 +1,4 @@
 import SubSideBar from "../SubSidebar";
-import { CSSTransition } from "react-transition-group";
-import styles from "./index.module.scss";
 import MainSideBar from "../MainSidebar";
 import useGenderOptions from "@/features/sidebar-navigation/hooks/useGenderOptions";
 
